a3a39622092ff0697c801c1d0f1bc5a7f9c2e901
Shrink <1363072460@qq.com>
2024-10-29 22:25:27 +0800

    2024.10.29
    使用了github desktop批量修改文件位置等

--- src/level2_1.java
+++
@@ -1,9 +0,0 @@
-//计算半径为 4 的圆的面积，计算公式为：面积=半径*半径*3.14。
-public class level2_1 {
-    public static double circle_area(double r){
-        return r * r * 3.14;
-    }
-    public static void main(String[] args) {
-        System.out.println(circle_area(4));
-    }
-}
